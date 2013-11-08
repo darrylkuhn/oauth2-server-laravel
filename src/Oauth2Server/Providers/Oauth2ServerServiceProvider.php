438c2ac48d6508e774ad28172aeaa5e05610c1fa
root <darryl.kuhn@gmail.com>
2013-11-08 14:25:27 -0800

    several key changes and bug fixes

--- src/Oauth2Server/Providers/Oauth2ServerServiceProvider.php
+++ src/Oauth2Server/Providers/Oauth2ServerServiceProvider.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\ServiceProvider;
 use \Config;
+use \Cache;
+use \DB;
+use \Exception;
+use \OAuth2;
+use \Redis;
 
 class Oauth2ServerServiceProvider extends ServiceProvider 
 {
@@ -14,25 +19,29 @@ class Oauth2ServerServiceProvider extends ServiceProvider
     {
         if ( ! Config::get('oauth2server') )
         {
-            throw new \Exception('Oauth2Server not configured. See: docs');
+            throw new Exception('Oauth2Server not configured. See: docs');
         }
 
         $this->app['Oauth2Server'] = $this->app->share(function($app)
         {
-            switch ( Config::get('oauth2server.storage_engine') )
-            { 
+            switch ( Config::get('oauth2server.storage_engine', 'pdo') )
+            {
                 case 'pdo':
                     $storage = new OAuth2\Storage\Pdo( 
-                        DB::connection(Config::get('oauth2server.connection_name'))->getPdo() 
+                        DB::connection(
+                            Config::get('oauth2server.connection_name', 
+                                Config::get('database.default')) )->getPdo() 
                     );
+                    break;
                 case 'redis':
-                    $storage = new OAuth2\Storage\Redis( Cache::connection() );
+                    $storage = new OAuth2\Storage\Redis( Redis::connection() );
+                    break;
                 default:
-                    throw new \Exception('No valid storage engine specified. Supported engines include: pdo and redis.');
+                    throw new Exception('No valid storage engine specified. Supported engines include: pdo and redis.');
                     break;
             }
 
-            return new OAuth2\Server($storage, Config::get('oauth2server') );
+            return new OAuth2\Server( $storage, Config::get('oauth2server') );
         });
     }
 
